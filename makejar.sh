9cffa5af6875f7f48cc9cb406e3ed33189d68c1f
RockerM4NHUN <zdancsek@citromail.hu>
2014-05-15 13:48:41 +0200

    Update makejar.sh

--- makejar.sh
+++ makejar.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-echo -n "Give jar name (without .jar): "
-read jname
-jname="$jname.jar"
-echo "$jname"
+if [ -z $1 ]
+then
+	echo -n "Give jar name (without .jar): "
+	read jname
+	jname="$jname.jar"
+else
+	jname="$1.jar"
+fi
 jar -cfm $jname manifest.txt CalendarProgram.class Res/*.class Res/*/*.class Res/*/*/*.class Res/*/*/*/*.class Res/*/*/*/*/*.class
